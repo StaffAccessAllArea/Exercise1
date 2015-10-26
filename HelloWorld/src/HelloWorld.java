6f18ef368feb8745a4114220080a8c3378665e75
StaffAccessAllArea <StaffAccessAllArea@users.noreply.github.com>
2015-10-26 23:45:12 +0100

    changed according to exercise sheet
    :wq

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 /**
  * 
  */
@@ -13,6 +15,22 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello ASE2015 - how are you? :)");
+		System.out.print("Please enter your name: ");
+		
+		byte buffer[] = new byte[80];  // Zeichenpuffer
+	    String input = "";
+		int read;
+		try {
+	          // Einlesen der Zeichen
+	          read = System.in.read(buffer, 0, 80);
+	          // Umwandeln des Pufferinhaltes in einen String
+	          input = new String(buffer, 0, read);
+	        }
+	        catch(IOException e) {
+	          e.printStackTrace();
+	        }
+		//name input;
+		System.out.println("Hello " + input);
 	}
 
 }
