1b1a420ae3a23ea2fda15c5e90614ce3f6701cf5
StaffAccessAllArea <StaffAccessAllArea@users.noreply.github.com>
2015-10-27 16:20:41 +0100

    newly created class file

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+
+public class HelloUser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello ASE2015 - how are you? :)");
+		System.out.print("Please enter your name: ");
+		
+		byte buffer[] = new byte[80];  // Zeichenpuffer
+	    String input = "";
+		int read;
+		try {
+	          // Einlesen der Zeichen
+	          read = System.in.read(buffer, 0, 80);
+	          // Umwandeln des Pufferinhaltes in einen String
+	          input = new String(buffer, 0, read);
+	        }
+	        catch(IOException e) {
+	          e.printStackTrace();
+	        }
+		//name input;
+		System.out.println("Hello " + input);
+	}
+	
+}
